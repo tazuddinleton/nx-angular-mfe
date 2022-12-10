47650a4638db753d31ce21077fd65a738e044865
taz <tazuddinleton@gmail.com>
2022-12-10 12:45:54 +0600

    Add html file

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -5,16 +5,7 @@ import { distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'mfe2-root',
-  template: `
-    <div class="dashboard-nav">Admin Dashboard</div>
-    <div *ngIf="isLoggedIn$ | async">
-      <button type="button" (click)="logOut()">Logout</button>
-    </div>
-    <div *ngIf="isLoggedIn$ | async; else signIn">
-      You are authenticated so you can see this content.
-    </div>
-    <ng-template #signIn><router-outlet></router-outlet></ng-template>
-  `,
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit{
   title = 'dashboard';
