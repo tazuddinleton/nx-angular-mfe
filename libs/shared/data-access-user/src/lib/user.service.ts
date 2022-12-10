a7c414736f01e36aa29919bb6c1c9cc7789270e6
taz <tazuddinleton@gmail.com>
2022-12-10 15:15:31 +0600

    Store login status in local storage

--- libs/shared/data-access-user/src/lib/user.service.ts
+++ libs/shared/data-access-user/src/lib/user.service.ts
@@ -6,14 +6,18 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class UserService {
 
-  private isUserLoggedIn = new BehaviorSubject(false);
+  private isUserLoggedIn = new BehaviorSubject(!!localStorage.getItem('login_status'));
   isUserLoggedIn$ = this.isUserLoggedIn.asObservable();
+
   checkCredentials(username: string, password: string) {
     if (username === 'demo' && password === 'demo') {
+      localStorage.setItem('login_status', 'true');
       this.isUserLoggedIn.next(true);
     }
   }
+
   logout() {
+    localStorage.removeItem('login_status');
     this.isUserLoggedIn.next(false);
   }
 }
